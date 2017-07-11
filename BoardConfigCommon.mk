9c36b808b7dc4f0966f62aa75d04c1afe3189b3f
ZIM <zeustreker@gmail.com>
2017-07-11 19:35:25 +0300

    Update setup Wizars and try fix crypt userspace

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,6 +128,7 @@ AUDIO_FEATURE_ENABLED_FM             := true
 
 # Enable HW based full disk encryption
 TARGET_HW_DISK_ENCRYPTION            := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE       := true
 
 # Power HAL
 TARGET_POWERHAL_SET_INTERACTIVE_EXT  := $(LOCAL_PATH)/power/power_ext.c
