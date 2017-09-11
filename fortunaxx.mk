cdafc5337e35f7b56cfb377e92fd411a14685f11
ZIM <zeustreker@gmail.com>
2017-09-11 14:16:42 +0300

    Return Texture config

--- fortunaxx.mk
+++ fortunaxx.mk
@@ -255,4 +255,7 @@ $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
 # Dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
+# Texture config.
+ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
